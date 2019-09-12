16736ba96fe85f765018e855430fd36972e6617f
Prashant Neupane <prashantneupane8848@gmail.com>
2019-09-12 17:00:52 -0500

    displays blocks

--- SortingVisualization/src/app/app.component.ts
+++ SortingVisualization/src/app/app.component.ts
@@ -1,23 +1,23 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Selection Sort';
   // ADD CHART OPTIONS. 
   chartOptions = {
     responsive: true,
     pointBorderWidth: 5,
   }
-
+  array = [21, 56, 4, 31, 45, 15, 57, 61, 9, 17, 24, 59, 47, 9, 28, 54, 77, 51, 24]
   labels =  [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
   // STATIC DATA FOR THE CHART IN JSON FORMAT.
   chartData = [{
-      data: [21, 56, 4, 31, 45, 15, 57, 61, 9, 17, 24, 59, 47, 9, 28, 54, 77, 51, 24],
+      data: this.array,
     },];
 
   // CHART COLOR.
