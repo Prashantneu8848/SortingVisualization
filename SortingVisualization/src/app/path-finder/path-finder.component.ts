efeceaf6a08ca0226fcc0e6287acdb65a5757689
Prashant Neupane <prashantneupane8848@gmail.com>
2020-04-13 20:13:46 -0500

    Deployment

--- SortingVisualization/src/app/path-finder/path-finder.component.ts
+++ SortingVisualization/src/app/path-finder/path-finder.component.ts
@@ -9,35 +9,66 @@ export class PathFinderComponent implements OnInit {
   @ViewChild('canvas', { static: true }) 
   canvas: ElementRef<HTMLCanvasElement>;
   rect: DOMRect;
+  canvasElem: HTMLCanvasElement;
+  sqaures: Square[];
   constructor() { }
 
   private ctx: CanvasRenderingContext2D;
 
   ngOnInit() {
     this.ctx = this.canvas.nativeElement.getContext('2d');
-    this.drawRects();
-    let canvasElem = document.querySelector("canvas");
-    canvasElem.addEventListener("mousedown", (e) => { this.getMousePosition(canvasElem, e);}); 
+    this.initializeSquares();
+    this.canvasElem = document.querySelector("canvas");
+    this.canvasElem.addEventListener("mousedown", (e: MouseEvent) => { this.getMousePosition(this.canvasElem, e);}); 
   }
 
   getMousePosition(canvasElem: HTMLCanvasElement, e: MouseEvent) {
     this.rect = canvasElem.getBoundingClientRect();
     let x = e.clientX - this.rect.left; 
     let y = e.clientY - this.rect.top; 
-    console.log("Coordinate x: " + x, "Coordinate y: " + y); 
+    let selectedSquare = -1;
+    for (var i = 0; i < this.sqaures.length; i++) {
+      let sq: Square = this.sqaures[i];
+      var dx = x - sq.x;
+      var dy = y - sq.y;
+      var zz = sq.z * sq.z;
+      if (dx * dy < zz) {
+          selectedSquare = i;
+          console.log(selectedSquare);
+      }
+    }
+    if (selectedSquare >= 0) {
+      var sq = this.sqaures[selectedSquare];
+      sq.x = x;
+      sq.y = y;
   }
 
-  drawRects(): void {
+    this.drawRects();
+  }
+
+  initializeSquares(): void {
     this.ctx.fillStyle = 'red';
-    
-    const square1 = new Square(this.ctx);
-    const square2 = new Square(this.ctx);
     let x1: number = 10;
     let x2: number = 700;
     let y1: number = 100;
     let y2: number = 300;
-    square1.draw(x1, y1, 15);
-    square2.draw(x2, y2, 15);
+    let r: number = 15;
+
+    const square1 = new Square(this.ctx, x1, y1, r);
+    const square2 = new Square(this.ctx, x2, y2, r);
+    this.sqaures = [square1,square2];
+    
+    square1.draw();
+    square2.draw();
+  }
+
+  drawRects(): void {
+    this.ctx.clearRect(0, 0, 800, 400);
+    this.ctx.fillStyle = 'red';
+    for (var i = 0; i < this.sqaures.length; i++) {
+      let sq: Square = this.sqaures[i];
+      sq.draw();
+    }
   }
 
   animate(): void {
@@ -46,9 +77,17 @@ export class PathFinderComponent implements OnInit {
 }
 
 export class Square {
-  constructor(private ctx: CanvasRenderingContext2D) {}
+  // z is height and width for the square
+  x: number;
+  y: number;
+  z: number;
+  constructor(private ctx: CanvasRenderingContext2D, x: number, y: number, z: number) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
 
-  draw(x: number, y: number, z: number) {
-    this.ctx.fillRect(x, y, z, z);
+  draw() {
+    this.ctx.fillRect(this.x, this.y, this.z, this.z);
   }
 }
